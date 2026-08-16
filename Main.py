8db5e5924144234e1208143bd94d49798a73bf5b
Julien <julien.faillon@student.isae-supaero.fr>
2026-08-16 20:34:30 +0200

    gitignore mis en place validé

--- Main.py
+++ Main.py
@@ -2,4 +2,4 @@ from Source.ImportJson import ImportJson
 
 
 link = ImportJson.construct_url(2025, 6, 12)
-ImportJson.import_file(link, "Data", "test.json")
+ImportJson.import_file(link, "data", "test.json")
